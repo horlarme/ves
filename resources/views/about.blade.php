c953ae33dadced447cffad1667004726d8feae04
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2018-08-17 13:18:38 +0100

    Completed About Page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -137,20 +137,20 @@
                 <div class="panel-body">
                     <div class="col-sm-12">
                         <p class="lead">Taking Nigeria to the Venice Biennale. <a
-                                    href="https://edition.cnn.com/videos/world/2017/04/20/african-voices-taking-nigeria-to-the-venice-biennale-b.cnn">CNN
+                                    href="https://edition.cnn.com/videos/world/2017/04/20/african-voices-taking-nigeria-to-the-venice-biennale-b.cnn" target="_blank">CNN
                                 Africa</a></p>
                         <p class="lead">Victor Ehikhamenor: Portraits from a world in transition. <a
-                                    href="http://edition.cnn.com/style/article/artist-fight-to-encourage-generation/index.html">CNN
+                                    href="http://edition.cnn.com/style/article/artist-fight-to-encourage-generation/index.html" target="_blank">CNN
                                 Style</a></p>
                         <p class="lead">Victor Ehikhamenor: Shaped by Memory and Tribal Tradition. <a
-                                    href="https://www.ft.com/content/a3544b56-ce34-11e4-86fc-00144feab7de">Financial
+                                    href="https://www.ft.com/content/a3544b56-ce34-11e4-86fc-00144feab7de" target="_blank">Financial
                                 Times</a></p>
                         <p class="lead">Nigerian artist Victor Ehikhamenor's canvas sculptures. <a
-                                    href="https://www.bbc.co.uk/news/av/world-africa-27597407/nigerian-artist-victor-ehikhamenor-s-canvas-sculptures">BBC
+                                    href="https://www.bbc.co.uk/news/av/world-africa-27597407/nigerian-artist-victor-ehikhamenor-s-canvas-sculptures" target="_blank">BBC
                                 World News – Africa</a></p>
                         <p class="lead">Sabo Kpade, Victor Ehikhamenor's Latest Work Explores Controversy Through
                             the Lens of Bini Spirituality. <a
-                                    href="http://www.okayafrica.com/victor-ehikhamenors-latest-is-a-response-to-controversy-and-a-intersection-of-benin-spirituality-and-christianity/">Okay
+                                    href="http://www.okayafrica.com/victor-ehikhamenors-latest-is-a-response-to-controversy-and-a-intersection-of-benin-spirituality-and-christianity/" target="_blank">Okay
                                 Africa</a></p>
                     </div>
                 </div>
@@ -165,22 +165,30 @@
             <div class="row panel-collapse collapse" id="sse">
                 <div class="panel-body">
                     <div class="col-sm-12">
-                        <p class="lead">2017 “In the Kingdom of this World”, Tyburn Gallery, London<br/><br />
-                            2016 1:54 Contemporary African Art Fair<br/><br />2015 “Paperwork: Works on paper” Constant
-                            Capital Gallery, Lagos<br/><br />2014 “Chronicles Of The Enchanted World” Gallery of African Art,
-                            London<br/><br />2014 “In The Lion’s Lair: Photographing Wole Soyinka” Ake Arts and Books
-                            Festival, Abeokuta, Nigeria<br/><br />2013 “Amusing The Muse” Temple Muse, Lagos<br/><br />2012
-                            "Crossing the Line" Bloom Gallery, Lagos<br/><br />2011 “Entrances & Exits: In search of not
-                            forgetting, CCA, Lagos Nigeria<br/><br />2011 “Artist Experience” Whitespace, Ikoyi Lagos<br/><br />2010
-                            “Roforofo Fight: Painting to Fela’s Music” Bloom Gallery, Lagos Nigeria<br/><br />2009 “Mirrors
-                            and Mirages” Terra Kulture Gallery, Lagos, Nigeria<br/><br />2008 “Invasion of Privacy” Jennings
-                            Gallery, Washington, D.C<br/><br />2007 “Rocks & Roses” Victoria Crown Plaza, Lagos, Nigeria.<br/><br />2007
-                            “Labyrinth of Memories” Didi Museum, Lagos, Nigeria.<br/><br />2006 “Beyond The River” Grenada
-                            Embassy, Washington, D.C.<br/><br />2005 “Body Language” Utopia Art/Grill, Washington, D.C.<br/><br />2005
-                            “Divine Intervention” Howard University A J Blackburn Center Gallery, Washington, D.C.<br/><br />2005
-                            “Talking Walls” BB&T Bank, NW Washington, D.C.<br/><br />2004 “Songs and Stories: Moonlight
-                            Delight” Utopia Gallery, Washington, D.C.<br/><br />2001 “Discovering the gods” Monroe Gallery,
-                            Arts Club of Washington, Washington, D.C.<br/><br />2000 “Spirits In Dialogue” The
+                        <p class="lead">2017 “In the Kingdom of this World”, Tyburn Gallery, London<br/><br/>
+                            2016 1:54 Contemporary African Art Fair<br/><br/>2015 “Paperwork: Works on paper” Constant
+                            Capital Gallery, Lagos<br/><br/>2014 “Chronicles Of The Enchanted World” Gallery of African
+                            Art,
+                            London<br/><br/>2014 “In The Lion’s Lair: Photographing Wole Soyinka” Ake Arts and Books
+                            Festival, Abeokuta, Nigeria<br/><br/>2013 “Amusing The Muse” Temple Muse, Lagos<br/><br/>2012
+                            "Crossing the Line" Bloom Gallery, Lagos<br/><br/>2011 “Entrances & Exits: In search of not
+                            forgetting, CCA, Lagos Nigeria<br/><br/>2011 “Artist Experience” Whitespace, Ikoyi
+                            Lagos<br/><br/>2010
+                            “Roforofo Fight: Painting to Fela’s Music” Bloom Gallery, Lagos Nigeria<br/><br/>2009
+                            “Mirrors
+                            and Mirages” Terra Kulture Gallery, Lagos, Nigeria<br/><br/>2008 “Invasion of Privacy”
+                            Jennings
+                            Gallery, Washington, D.C<br/><br/>2007 “Rocks & Roses” Victoria Crown Plaza, Lagos, Nigeria.<br/><br/>2007
+                            “Labyrinth of Memories” Didi Museum, Lagos, Nigeria.<br/><br/>2006 “Beyond The River”
+                            Grenada
+                            Embassy, Washington, D.C.<br/><br/>2005 “Body Language” Utopia Art/Grill, Washington,
+                            D.C.<br/><br/>2005
+                            “Divine Intervention” Howard University A J Blackburn Center Gallery, Washington,
+                            D.C.<br/><br/>2005
+                            “Talking Walls” BB&T Bank, NW Washington, D.C.<br/><br/>2004 “Songs and Stories: Moonlight
+                            Delight” Utopia Gallery, Washington, D.C.<br/><br/>2001 “Discovering the gods” Monroe
+                            Gallery,
+                            Arts Club of Washington, Washington, D.C.<br/><br/>2000 “Spirits In Dialogue” The
                             Brazilian-American Cultural Institute Gallery, Washington, D.C.<br/br />2000 “Beyond The
                             Surface” Utopia Art/Grill, Washington, D.C</p>
                     </div>
@@ -189,12 +197,134 @@
 
             <div class="text-center panel-heading">
                 <h3 class="panel-title">
-                    <a href="#" class="panel-title panel-toggle collapsed" data-toggle="collapse">Biography</a></h3>
+                    <a href="#sge" class="panel-title panel-toggle collapsed" data-toggle="collapse">Selected Group
+                        Exhibitions</a></h3>
             </div>
-            <div class="row panel-collapse collapse" id="">
+            <div class="row panel-collapse collapse" id="sge">
                 <div class="panel-body">
                     <div class="col-sm-12">
-                        <p class="lead"></p>
+                        <p class="lead">2017 “A Biography of the Forgotten”, Nigerian Pavilion, 57thVenice Biennale,
+                            Venice, Italy<br/><br/>
+                            2016 Dakart Biennale Curated by Simon Njami<br/><br/>
+                            2016 “A Place In Time” Nirox Foundation Nature Park Sculpture<br/><br/>
+                            2015 “Displacement” Fiction and drawings in collaboration with Toby Zielony for German
+                            Pavilion, 56th Venice Biennale, Italy<br/><br/>
+                            2015 Biennale Jogja XIII, Equator #3 2015, Indonesia
+                            <br/><br/>2014 “ARENA” Center of Contemporary Art, Torun, Poland
+                            <br/><br/>2013 “Nigeria Now” Ake Arts & Book Festival World Bank Exhibition, Nigeria
+                            <br/><br/>2012 “Post colonial photo studio” Action Field Kodra, Thessaloniki, Greece
+                            <br/><br/>2008 “Black Creativity” Museum of Science and Industry, Chicago, IL, USA
+                            <br/><br/>2007 “Rhythms & Blues” Sarah Silberman Gallery, Montgomery College, MD, USA
+                            <br/><br/>2004 “Memories: 2Griot” JoySmith Gallery, Memphis, TN, USA
+                            <br/><br/>2003 “Symbols and Metaphors” Underwood Studio, Chevy Chase, MD, USA
+                            <br/><br/>2002 Home of African Concepts Gallery, Kent, WA, USA
+                            <br/><br/>2001 Patuxent Art League Members Juried Show, Laurel, MD, USA
+                            <br/><br/>2001 “Same Tree, Different Roots” Howard University AJ Blackburn Center Gallery,
+                            Washington, D.C., USA
+                            <br/><br/>1999 National Geographic Society Staff Show, Washington D.C. ,USA
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center panel-heading">
+                <h3 class="panel-title">
+                    <a href="#award" class="panel-title panel-toggle collapsed" data-toggle="collapse">Awards/Residencies</a>
+                </h3>
+            </div>
+            <div class="row panel-collapse collapse" id="award">
+                <div class="panel-body">
+                    <div class="col-sm-12">
+                        <p class="lead">
+                            2018 Art Dubai Residency, Dubai
+                            <br/><br/>2018 Civitella Ravieri Fellow, Italy
+                            <br/><br/>2016 Rockefeller Foundation Bellagio Fellow, Italy
+                            <br/><br/>2016 Nirox Foundation Residency, Johannesburg, South Africa
+                            <br/><br/>2016 Greatmore Residency, Cape Town, South Africa
+                            <br/><br/>2015 Casa Zia Lina, Elba Italy 2014 Norman Mailer Fellowship for Writing
+                            <br/><br/>2008 Leon Forrest Africana Scholar Award
+                            <br/><br/>2006 Familiar Streets Writing Contest Award for “A Beautiful Freedom” by Hyman S.
+                            & Freda Bernstein Jewish Literary Festival, Washington, DC.
+                            <br/><br/>2003 Highly Commended Award for “The Supreme Command”, Commonwealth Broadcasting
+                            Association, London, UK
+                            <br/><br/>2000 1st Place, Arts Club of Washington Juried Exhibition. Juror, Dr. Kimberly A.
+                            Jones, National Gallery of Art, Washington D.C.
+                            <br/><br/>1999 Finalist for “Palace Dwellers” The Artist’s Magazine Art Competition
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center panel-heading">
+                <h3 class="panel-title">
+                    <a href="#spwr" class="panel-title panel-toggle collapsed" data-toggle="collapse">Selected Published
+                        Works and Reviews</a></h3>
+            </div>
+            <div class="row panel-collapse collapse" id="spwr">
+                <div class="panel-body">
+                    <div class="col-sm-12">
+                        <p class="lead">2015 “Shaped By Memory and Tribal Tradition” By Liz Bolshaw, Financial Times
+                            <br/><br/>2015 <a href="https://www.youtube.com/watch?v=1xsXQ0B8p6g" target="_blank">CCTV, China</a>
+                            <br/><br/>2015 <a href="http://www.bbc.com/news/world-africa-31730010" target="_blank">BBC World</a>
+                            <br/><br/>2014 <a href="http://www.bbc.com/news/world-africa-27597407" target="_blank">BBC World - Africa</a>
+                            <br/><br/>2014 <a href="http://www.bloombergtvafrica.com/video/africa-trending-investingafrican-art/" target="_blank">Bloomberg Review</a>
+                            <br/><br/>2014 <a href="http://africanah.org/victor-ehikhamenor/" target="_blank">A review</a>
+                            <br/><br/>2014 <a href="https://www.youtube.com/watch?v=5l2g_iGmiKI" target="_blank">Video installation Center for Contemporary Art, Lagos, Nigeria</a>
+                            <br/><br/>2014 <a href="http://vimeo.com/100910371" target="_blank">Short documentary by Celia Atonaci</a>
+                            <br/><br/>2014 “Confronting An Empty Canvas” AfricanWriter.com
+                            <br/><br/>2014 “Walking With Wole Soyinka” Essay. Africasacountry.com
+                            <br/><br/>2014 “Blindness At The Top” Essay. New York Times.
+                            <br/><br/>2013 “Africa’s voice, Nigeria’s conscience” Essay. New York Times.
+                            <br/><br/>2013 “Is Nigeria At War?” CNN Online
+                            <br/><br/>2013 Excuse Me! Collection Of Selected Essays. Parresia Publishers, Lagos, Nigeria
+                            <br/><br/>2010 “A Picture From Ireland” AGNI magazine, Boston University, USA
+                            <br/><br/>2008 Of Money, Friends, and Greed,. Anthology of short stories. Hodder Publishers,
+                            UK
+                            <br/><br/>2008 “Riding Monkey With Pako” Per Contra magazine, USA
+                            <br/><br/>2008 “Passport to Heaven” Farafina Magazine, Lagos, Nigeria
+                            <br/><br/>2007 “Glory Halleluya” and “Woman in Trance” Stylus Magazine, University of
+                            Maryland, University College
+                            <br/><br/>2006 “Who Will Bury the Dead” Wasafiri Magazine, University of London, UK
+                            <br/><br/>2006 “Echoes From the Valley” NTA Documentary, Nigeria and USA
+                            <br/><br/>2003 “The Supreme Command” CBA London, UK
+                            <br/><br/>“Between The Muse and Love of Fatherland” Daily Independent, Lagos Nigeria.
+                            <br/><br/>“Homeland in the Eyes of an Exile” Guardian Newspaper, Lagos Nigeria.
+                            <br/><br/>Multicultural Ethnic Festival, (poetry performance) Dan Hague, Netherlands.
+                            <br/><br/>POETRYFEST by TransAfrican Forum, Washington DC
+
+                            <br/><br/>2002 Sordid Rituals, book of poems and drawings. Published by Oracle Books, Lagos,
+                            Nigeria.
+                            <br/><br/>2001 “Introduction” Konch E-Magazine “Life is <br/><br/>Short, Autobiography as
+                            Haiku” Style section, The Washington Post </p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center panel-heading">
+                <h3 class="panel-title">
+                    <a href="#last" class="panel-title panel-toggle collapsed" data-toggle="collapse">Selected Book
+                        Cover Designs:y</a></h3>
+            </div>
+            <div class="row panel-collapse collapse" id="last">
+                <div class="panel-body">
+                    <div class="col-sm-12">
+                        <p class="lead">2015 Story Collection for Caine Prize for African Writing 2015
+                            <br/><br/>2014 Americanah by Chimamanda Ngozi Adichie
+                            <br/><br/>2014 Half Of A Yellow Sun- (Movie Edition) by Chimamanda Adichie
+                            <br/><br/>2014 Africa in Fragments: Essays on Nigeria, Africa and Africanity by Moses Ochonu
+                            <br/><br/>2013 Oil on Water by Helon Habila
+                            <br/><br/>2010 Christopher Okigbo: Thirsting for Sunlight by Obi Nwakanma
+                            <br/><br/>2009 Salutes without Guns collection of poems by Ikeogu Oke
+                            <br/><br/>2009 Songs of Absence and Despair collection of poems by Toni Kan
+                            <br/><br/>2008 Of Friends, Money and Greed Anthology of three stories and a play by Hodders
+                            Publishers, UK
+                            <br/><br/>2008 One World Anthology of short stories by New Internationlist Publishers, UK
+                            <br/><br/>2008 Jambula Tree and Other Stories Anthology of Caine Prize winners and
+                            shortlisted writer by Cassava Republic, Abuja Nigeria
+                            <br/><br/>2007 Measuring Times by Helon Habila
+                            <br/><br/>2005 English In Africa Journal of the English Department, Rhodes University, South
+                            Africa
+                            <br/><br/>2005 Sky High Flame by Unoma Azuah
+                            <br/><br/>2004 Purple Hibiscus by Chimamanda Adichie
+                            <br/><br/>2003 Feeding Frenzy by Jonathan Luckett
+                        </p>
                     </div>
                 </div>
             </div>
