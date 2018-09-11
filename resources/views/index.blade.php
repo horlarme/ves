3fde7441d9a9a92393ade650ec45b9bda3b411e7
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2018-09-11 13:59:24 +0100

    Removed link from slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
                                  data-start="700"
                                  data-easing="Back.easeOut"
                                  data-endspeed="300"
-                                 data-endeasing="Power1.easeIn"><a href="{{route('prayer_page')}}">Victor Ehikhamenor</a></div>
+                                 data-endeasing="Power1.easeIn">Victor Ehikhamenor</div>
                             <div class="caption slab customin customout opacity-bg" data-x="center" data-y="264" data-customin="x:0;y:0;z:0;rotationX:90;rotationY:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
                                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
                                  data-speed="600"
